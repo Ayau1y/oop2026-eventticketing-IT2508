751890024ee6d7354a5a934f1f6ecc68fe6afe79
Ru1nE4 <253006@astanait.edu.kz>
2026-02-04 02:10:20 +0500

    Add Azamat

--- src/ticketing/entities/StandardTicket.java
+++ src/ticketing/entities/StandardTicket.java
@@ -1,6 +1,6 @@
 package ticketing.entities;
 
-public class StandardTicket extends Ticket {
+public class StandardTicket  extends Ticket {
 
     public StandardTicket(int id, String code, int eventId, int seatId,
                           Customer customer, double price) {
